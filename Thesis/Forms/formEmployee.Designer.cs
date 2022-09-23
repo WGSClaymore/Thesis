7caf5d3a59cf87855cbe91c0a62afee074eb3bb7
OcimOg0617 <57973388+OcimOg0617@users.noreply.github.com>
2022-09-23 23:19:52 +0800

    Complaint and employee forms added

---
+++ Thesis/Forms/formEmployee.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace Thesis
+{
+    partial class formEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.Area = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SG = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Item = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Pos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EmpName = new System.Windows.Forms.TextBox();
+            this.Empdgv = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Empdgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(73, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 20);
+            this.label5.TabIndex = 64;
+            this.label5.Text = "Area of Assignment:";
+            // 
+            // Area
+            // 
+            this.Area.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Area.Location = new System.Drawing.Point(237, 170);
+            this.Area.Name = "Area";
+            this.Area.Size = new System.Drawing.Size(214, 27);
+            this.Area.TabIndex = 63;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(151, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 20);
+            this.label4.TabIndex = 62;
+            this.label4.Text = "SG/Step:";
+            // 
+            // SG
+            // 
+            this.SG.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SG.Location = new System.Drawing.Point(237, 137);
+            this.SG.Name = "SG";
+            this.SG.Size = new System.Drawing.Size(214, 27);
+            this.SG.TabIndex = 61;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(156, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 60;
+            this.label3.Text = "Item No:";
+            // 
+            // Item
+            // 
+            this.Item.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Item.Location = new System.Drawing.Point(237, 104);
+            this.Item.Name = "Item";
+            this.Item.Size = new System.Drawing.Size(214, 27);
+            this.Item.TabIndex = 59;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(156, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 58;
+            this.label2.Text = "Position:";
+            // 
+            // Pos
+            // 
+            this.Pos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pos.Location = new System.Drawing.Point(237, 71);
+            this.Pos.Name = "Pos";
+            this.Pos.Size = new System.Drawing.Size(214, 27);
+            this.Pos.TabIndex = 57;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "Name:";
+            // 
+            // EmpName
+            // 
+            this.EmpName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmpName.Location = new System.Drawing.Point(237, 38);
+            this.EmpName.Name = "EmpName";
+            this.EmpName.Size = new System.Drawing.Size(214, 27);
+            this.EmpName.TabIndex = 55;
+            // 
+            // Empdgv
+            // 
+            this.Empdgv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Empdgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Empdgv.Location = new System.Drawing.Point(481, 23);
+            this.Empdgv.Name = "Empdgv";
+            this.Empdgv.RowHeadersWidth = 51;
+            this.Empdgv.RowTemplate.Height = 24;
+            this.Empdgv.Size = new System.Drawing.Size(852, 405);
+            this.Empdgv.TabIndex = 54;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(176, 360);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 41);
+            this.button4.TabIndex = 53;
+            this.button4.Text = "HOME";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(299, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 41);
+            this.button3.TabIndex = 52;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(176, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 41);
+            this.button2.TabIndex = 51;
+            this.button2.Text = "EDIT";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(53, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 41);
+            this.button1.TabIndex = 50;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // formEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1387, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Area);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SG);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Item);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Pos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EmpName);
+            this.Controls.Add(this.Empdgv);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "formEmployee";
+            this.Text = "formEmployee";
+            ((System.ComponentModel.ISupportInitialize)(this.Empdgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Area;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox SG;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Item;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Pos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EmpName;
+        private System.Windows.Forms.DataGridView Empdgv;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
